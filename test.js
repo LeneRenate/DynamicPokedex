9c29f007c6401e5f4d6a5ae912c85124e5b204eb
LeneRenate <Lene.renate.rosseboe@gmail.com>
2025-11-03 17:33:35 +0100

    updated image names

--- test.js
+++ test.js
@@ -1,20 +1,39 @@
 const API_BASE = "https://pokeapi.co/api/v2/pokemon";
 
-async function getPokemonData(nameOrId) {
-  const res = await fetch(`${API_BASE}/${nameOrId}`);
-  const data = await res.json();
-  return data;
+/** Fetch a single Pokémon by name or ID */
+async function fetchPokemon(nameOrId) {
+  const res = await fetch(`${API_BASE}/${nameOrId.toLowerCase()}`);
+  return await res.json(); // returns full Pokémon object
 }
 
-console.log(await getPokemonData("50"));
-// const pokemonData = await getPokemonData();
+/** Fetch the first n Pokémon as an array of objects */
+async function fetchFirstNPokemon(n) {
+  const ids = Array.from({ length: n }, (_, i) => i + 1);
 
-// const pokemonDetails = await Promise.all(
-//   pokemonData.results.map(async (pokemon) => {
-//     const res = await fetch(pokemon.url);
-//     const data = await res.json();
-//     return data;
-//   })
-// );
+  // Fetch all in parallel
+  const pokemonArray = await Promise.all(ids.map((id) => fetchPokemon(id)));
 
-// pokemonDetails.map((opkemon) => console.log(opkemon));
+  // Return array of Pokémon objects
+  return pokemonArray;
+}
+
+/** Example usage */
+async function example() {
+  // Get the first 10 Pokémon
+  const testArray = await fetchFirstNPokemon(6);
+
+  // Iterate easily
+  firstTen.forEach((p) => {
+    console.log(p.name); // "bulbasaur", "ivysaur", etc.
+    console.log(p.id); // Pokémon ID
+    console.log(p.types); // Array of type objects
+    console.log(p.sprites.front_default); // sprite image
+  });
+
+  // Fetch a specific Pokémon by name
+  const pikachu = await fetchPokemon("pikachu");
+  console.log(pikachu);
+}
+
+const pikachu = await fetchPokemon("122");
+console.log(pikachu);
