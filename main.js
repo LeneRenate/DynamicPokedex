b2837dc195176a0fa6dea17fba8e4658233770c4
LeneRenate <Lene.renate.rosseboe@gmail.com>
2026-01-04 22:55:55 +0100

    Render all pokemons, but start by showing just gen 1

--- main.js
+++ main.js
@@ -20,10 +20,23 @@ import { renderPokemon } from "./src/render.js";
 import { renderTypes, filterByType } from "./src/types.js";
 import { activateModal } from "./src/modal.js";
 import { genToggle } from "./src/generations.js";
+import { applyFilters, selectedGens } from "./src/filters.js";
 
 async function main() {
   // Start function
-  await renderPokemon(386); // Render the pokemonCards once
+  await renderPokemon(1025); // Render all the pokemonCards once
+
+  // Only show gen 1 on load, with active toggle on gen1-btn
+  selectedGens.push(1);
+  applyFilters();
+
+  const gen1Btn = document.querySelector(`.genBtn[data-gen="1"]`);
+  if (gen1Btn) {
+    gen1Btn.classList.add(`active`);
+    gen1Btn.style.backgroundColor = "seagreen";
+    gen1Btn.style.boxShadow = "0px 0px 5px mediumseagreen";
+  }
+
   await renderTypes(); // Render the typeBtns once
   filterByType(); // Add eventlisteners to all typeBtns
   activateModal(); // Add eventlisteners to all pokemonCards
